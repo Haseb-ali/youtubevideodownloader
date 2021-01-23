052ed381e79e0de9ad3b30aa206fcdd848dd140c
HASEEB <engineer.haseb@gmail.com>
2021-01-23 14:17:16 +0500

    Adding gui interface

--- downloader.py
+++ downloader.py
@@ -1,13 +1,17 @@
 from pytube import YouTube
 from pytube.cli import  on_progress
+from tkinter import *
 import sys
-while(True):
-    try:
-            video_url=input("Enter youtube video url: = ")
-            yt=YouTube(video_url,on_progress_callback=on_progress)
-            print(yt.title,"Downloading......")
-            print("video lenght :",yt.length,'Seconds')
-            yt.streams.filter(progressive=True,file_extension='mp4').order_by("resolution").desc().first().download()
-            print(yt.title,"downloading completed...")
-    except:
-        print("Error:= ",sys.exc_info())
+def load_dowload_gui():
+    pass
+def downlaod_video():
+    while(True):
+        try:
+                video_url=input("Enter youtube video url: = ")
+                yt=YouTube(video_url,on_progress_callback=on_progress)
+                print(yt.title,"Downloading......")
+                print("video lenght :",yt.length,'Seconds')
+                yt.streams.filter(progressive=True,file_extension='mp4').order_by("resolution").desc().first().download()
+                print(yt.title,"downloading completed...")
+        except:
+            print("Error:= ",sys.exc_info())
